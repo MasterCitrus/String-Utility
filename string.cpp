aea1b36b9f5afe071d54d21a796da9cb0848721c
MasterCitrus <45452554+MasterCitrus@users.noreply.github.com>
2024-02-14 08:13:04 +1100

    Header and class progress

---
+++ string.cpp
@@ -0,0 +1,18 @@
+#include "String.h"
+#include <iostream>
+
+String::String() : _str{ nullptr } {
+
+}
+
+String::String(const char* _str) {
+
+}
+
+String::String(const String& _other) {
+
+}
+
+String::~String() {
+
+}

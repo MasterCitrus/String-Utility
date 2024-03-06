0290c4979c828567a7f45eb143550df4c76acb51
MasterCitrus <45452554+MasterCitrus@users.noreply.github.com>
2024-03-07 09:44:55 +1100

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "String.h"
-#include <string.h>
-#include <iostream>
-#include <cassert>
 
 int main() {
-	String string = "Hello World!"
-
-
-		std::cout << string << std::endl;
+	String string = "Hello World!";
+	std::cout << string << std::endl;
 }

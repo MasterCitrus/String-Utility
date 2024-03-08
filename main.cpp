469593e252146719405d5e8d622f674b90d9802c
MasterCitrus <45452554+MasterCitrus@users.noreply.github.com>
2024-03-08 14:04:20 +1100

    Added prompt

--- main.cpp
+++ main.cpp
@@ -3,4 +3,10 @@
 int main() {
 	String string = "Hello World!";
 	std::cout << string << std::endl;
+
+	std::cout << "\nPress 'Enter' to exit.";
+
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+	std::cin.get();
 }
